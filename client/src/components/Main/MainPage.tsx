c12c41dbc9a753daa23eb4afca32ed8bd3ca2e72
Hiirsh <47283490+Hiirsh@users.noreply.github.com>
2023-03-03 22:37:54 +0200

    async functions - 1

--- client/src/components/Main/MainPage.tsx
+++ client/src/components/Main/MainPage.tsx
@@ -2,25 +2,27 @@ import React from "react";
 import { Button, Card } from "react-bootstrap";
 import Posts from "./MainPage/Posts";
 import { PageNumbers } from "./MainPage/PageNumbers";
-import { ModalInputEnum, base_url } from "../../utils/constants";
+import { ModalInputEnum } from "../../utils/constants";
 import { usePosts } from "../../hooks/usePosts";
 import { ModalInput } from "../Modals/ModalInput";
+import { getPostsByPage } from "../../utils/asyncFunctions";
 
 export default function MainPage() {
   const { setPosts, setCurrentPage, setTotalPages } = usePosts();
   const [showAddPost, setShowAddPost] = React.useState(false);
 
-  const getPosts = async (currentPage = 1) => {
-    const responce = await fetch(`${base_url}/post/page/${currentPage}`);
-    const data = await responce.json();
-    setPosts(data.result);
-    setCurrentPage(data.page);
-    setTotalPages(data.totalPages);
-  };
-
+  const getPosts = async() =>
+    //@ts-ignore
+    await getPostsByPage({
+      setPosts,
+      setCurrentPage,
+      setTotalPages,
+    });
+    
   const handleAddPostModalShow = () => {
     setShowAddPost(!showAddPost);
   };
+
   React.useEffect(() => {
     getPosts();
   });
@@ -33,13 +35,17 @@ export default function MainPage() {
           <Card.Text>
             <Button onPointerDown={handleAddPostModalShow}>Add post</Button>
           </Card.Text>
-            <Posts getPosts={getPosts} />
+          <Posts getPosts={getPosts} />
           <Card.Footer>
             <PageNumbers updatePages={getPosts} />
           </Card.Footer>
         </Card.Body>
       </Card>
-      <ModalInput show={showAddPost} setShow={handleAddPostModalShow} type={ModalInputEnum.addPost}/>
+      <ModalInput
+        show={showAddPost}
+        setShow={handleAddPostModalShow}
+        type={ModalInputEnum.addPost}
+      />
     </div>
   );
 }
